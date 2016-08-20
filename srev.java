3a73051f083e122258ca80f88508fcb89a41f668
PAVITHRA-M <pavithram938@gmail.com>
2016-08-20 16:57:22 +0530

    Add files via upload

---
+++ srev.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Srev 
+{
+    public static void main(String[] args)
+ {
+        String s;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the string");
+        s=in.nextLine();
+        StringBuffer sb=new StringBuffer(s);
+        System.out.println(sb.reverse());
+        
+        
+    }
+    
+}
