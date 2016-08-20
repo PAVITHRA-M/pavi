384fa018321030019737b625e7115501297a7af7
PAVITHRA-M <pavithram938@gmail.com>
2016-08-20 18:05:47 +0530

    Add files via upload

---
+++ strcon .java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class strcon {
+
+    public static void main(String[] args) {
+     String s,s1;
+     Scanner in=new Scanner(System.in);
+     System.out.println("Enter the string");
+     s=in.nextLine();
+     s1=in.nextLine();
+      System.out.println(s+" "+s1);
+     
+     
+    }
+    
+}
