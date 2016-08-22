9c3e5d6c4ec7afc978aed0bc3353d69694cdee62
PAVITHRA-M <pavithram938@gmail.com>
2016-08-22 17:35:51 +0530

    Add files via upload

---
+++ stringcon.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class stringcon {
+
+    public static void main(String[] args) {
+     String s,s1,s2;
+     Scanner in=new Scanner(System.in);
+     System.out.println("Enter the string");
+     s=in.nextLine();
+     s1=in.nextLine();
+     s2=s+s1;
+      System.out.println(s2);
+     
+     
+    }
+    
+}
