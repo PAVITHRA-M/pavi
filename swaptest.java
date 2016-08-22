97119d10dc42231e97ddbcf9a1d9b9dc071554ee
PAVITHRA-M <pavithram938@gmail.com>
2016-08-22 17:22:51 +0530

    Add files via upload

---
+++ swaptest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Swaptest {
+
+    public static void main(String[] args) {
+       int n1,n2,n3;
+       Scanner in=new Scanner(System.in);
+       System.out.println("Enter the numbers");
+       n1=in.nextInt();
+       n2=in.nextInt();
+       n3=(n1+n2)-(n1=n2);
+       System.out.println(n1);
+        System.out.println(n3);
+       
+       
+    }
+    
+}
