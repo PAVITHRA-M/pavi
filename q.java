9bd32f05a3b13abaf430895ce4c78fdf946232b9
PAVITHRA-M <pavithram938@gmail.com>
2016-08-24 19:22:19 +0530

    Add files via upload

---
+++ q.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class Q {
+
+    
+    public static void main(String[] args) throws IOException{
+     
+       char c;
+       c=(char)System.in.read();
+       if(c!='q')
+       {
+         System.out.println("Exitfrom the program");
+       }
+               
+    }
+    
+}
