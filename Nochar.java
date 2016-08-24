5e45acbcd1ac7493ffdcb3721777bd2893ac9ea2
PAVITHRA-M <pavithram938@gmail.com>
2016-08-24 19:04:35 +0530

    Add files via upload

---
+++ Nochar.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Nochar {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s;
+      int c;
+      System.out.println("Eneter the string");
+      s=in.nextLine();
+       c=s.length();
+              System.out.println(c);
+    }
+    
+}
